8481ba427b1e33310734826b8cd0d601e241bf54
Micha <micha-1@fantasymail.de>
2007-03-19 00:59:57 +0100

    more event functions

--- clib/oevent.cpp
+++ clib/oevent.cpp
@@ -12,5 +12,126 @@ extern "C" {
         CAMLreturn(Val_int(fltk::event_dy()));
     }
 
+    CAMLprim value event_button(value n)
+    {
+        CAMLparam1(n);
+        CAMLreturn(Val_int(fltk::event_button()));
+    }
+
+    CAMLprim value event_set_clicks(value n)
+    {
+        CAMLparam1(n);
+        fltk::event_clicks(Int_val(n));
+        CAMLreturn(Val_unit);
+    }
+
+    CAMLprim value event_get_clicks(value n)
+    {
+        CAMLparam1(n);
+        CAMLreturn(Val_int(fltk::event_clicks()));
+    }
+
+    CAMLprim value event_is_click(value n)
+    {
+        CAMLparam1(n);
+        CAMLreturn(Val_int(fltk::event_is_click()));
+    }
+
+    CAMLprim value event_set_is_click(value n)
+    {
+        CAMLparam1(n);
+        fltk::event_is_click(Int_val(n));
+        CAMLreturn(Val_unit);
+    }
+
+    /*
+    static unsigned keys[] =
+    { 
+        fltk::LeftButton, 
+        fltk::MiddleButton, 
+        fltk::RightButton, 
+        fltk::SpaceKey, 
+        fltk::BackSpaceKey, 
+        fltk::TabKey, 
+        fltk::ClearKey, 
+        fltk::ReturnKey, 
+        fltk::PauseKey, 
+        fltk::ScrollLockKey, 
+        fltk::EscapeKey, 
+        fltk::HomeKey, 
+        fltk::LeftKey, 
+        fltk::UpKey, 
+        fltk::RightKey, 
+        fltk::DownKey, 
+        fltk::PageUpKey, 
+        fltk::PageDownKey, 
+        fltk::EndKey, 
+        fltk::PrintKey, 
+        fltk::InsertKey, 
+        fltk::MenuKey, 
+        fltk::HelpKey, 
+        fltk::NumLockKey, 
+        fltk::Keypad, 
+        fltk::KeypadEnter, 
+        fltk::MultiplyKey, 
+        fltk::AddKey, 
+        fltk::SubtractKey, 
+        fltk::DecimalKey, 
+        fltk::DivideKey, 
+        fltk::Keypad0, 
+        fltk::Keypad1, 
+        fltk::Keypad2, 
+        fltk::Keypad3, 
+        fltk::Keypad4, 
+        fltk::Keypad5, 
+        fltk::Keypad6, 
+        fltk::Keypad7, 
+        fltk::Keypad8, 
+        fltk::Keypad9, 
+        fltk::KeypadLast, 
+        fltk::F0Key, 
+        fltk::F1Key, 
+        fltk::F2Key, 
+        fltk::F3Key, 
+        fltk::F4Key, 
+        fltk::F5Key, 
+        fltk::F6Key, 
+        fltk::F7Key, 
+        fltk::F8Key, 
+        fltk::F9Key, 
+        fltk::F10Key, 
+        fltk::F11Key, 
+        fltk::F12Key, 
+        fltk::LastFunctionKey, 
+        fltk::LeftShiftKey, 
+        fltk::RightShiftKey, 
+        fltk::LeftCtrlKey, 
+        fltk::RightCtrlKey, 
+        fltk::CapsLockKey, 
+        fltk::LeftMetaKey, 
+        fltk::RightMetaKey, 
+        fltk::LeftAltKey, 
+        fltk::RightAltKey, 
+        fltk::DeleteKey, 
+    };
+    */
+
+    CAMLprim value get_special_keys(value n)
+    {
+        CAMLparam1(n);
+        CAMLlocal1(k);
+        k = caml_alloc_small(4,0);
+        Field(k,0) = fltk::LeftAccKey;
+        Field(k,1) = fltk::RightAccKey;
+        Field(k,2) = fltk::LeftCmdKey;
+        Field(k,3) = fltk::RightCmdKey;
+        CAMLreturn(k);
+    }
+
+    CAMLprim value event_key(value n)
+    {
+        CAMLparam1(n);
+        CAMLreturn(Val_int(fltk::event_key()));
+    }
 
 }
--- olib/event.ml
+++ olib/event.ml
@@ -6,4 +6,92 @@ type event_type =
    TOOLTIP
 ;;
 
+module Keys = struct
+    let leftButton	= 1;;
+    let middleButton	= 2;;
+    let rightButton	= 3;;
+    let spaceKey	= 32;;
+  (* 'a'-'z' *)
+    let backSpaceKey	= 0xff08;;
+    let tabKey	= 0xff09;;
+    let clearKey	= 0xff0b;;
+    let returnKey	= 0xff0d;;
+    let pauseKey	= 0xff13;;
+    let scrollLockKey	= 0xff14;;
+    let escapeKey	= 0xff1b;;
+    let homeKey	= 0xff50;;
+    let leftKey	= 0xff51;;
+    let upKey		= 0xff52;;
+    let rightKey	= 0xff53;;
+    let downKey	= 0xff54;;
+    let pageUpKey	= 0xff55;;
+    let pageDownKey	= 0xff56;;
+    let endKey	= 0xff57;;
+    let printKey	= 0xff61;;
+    let insertKey	= 0xff63;;
+    let menuKey	= 0xff67;;
+    let helpKey	= 0xff68;;
+    let numLockKey	= 0xff7f;;
+    let keypad	= 0xff80;;
+    let keypadEnter	= keypad+(int_of_char '\r');;
+    let multiplyKey	= keypad+(int_of_char '*');;
+    let addKey	= keypad+(int_of_char '+');;
+    let subtractKey	= keypad+(int_of_char '-');;
+    let decimalKey	= keypad+(int_of_char '.');;
+    let divideKey	= keypad+(int_of_char '/');;
+    let keypad0	= keypad+(int_of_char '0');;
+    let keypad1	= keypad+(int_of_char '1');;
+    let keypad2	= keypad+(int_of_char '2');;
+    let keypad3	= keypad+(int_of_char '3');;
+    let keypad4	= keypad+(int_of_char '4');;
+    let keypad5	= keypad+(int_of_char '5');;
+    let keypad6	= keypad+(int_of_char '6');;
+    let keypad7	= keypad+(int_of_char '7');;
+    let keypad8	= keypad+(int_of_char '8');;
+    let keypad9	= keypad+(int_of_char '9');;
+    let keypadLast	= 0xffbd;;
+    let f0Key		= 0xffbd;;
+    let f1Key		= f0Key+1;;
+    let f2Key		= f0Key+2;;
+    let f3Key		= f0Key+3;;
+    let f4Key		= f0Key+4;;
+    let f5Key		= f0Key+5;;
+    let f6Key		= f0Key+6;;
+    let f7Key		= f0Key+7;;
+    let f8Key		= f0Key+8;;
+    let f9Key		= f0Key+9;;
+    let f10Key	= f0Key+10;;
+    let f11Key	= f0Key+11;;
+    let f12Key	= f0Key+12;;
+    (* use f0Key+n to get function key n *)
+    let lastFunctionKey = f0Key+35;;
+    let leftShiftKey	= 0xffe1;;
+    let rightShiftKey	= 0xffe2;;
+    let leftCtrlKey	= 0xffe3;;
+    let rightCtrlKey	= 0xffe4;;
+    let capsLockKey	= 0xffe5;;
+    let leftMetaKey	= 0xffe7;;
+    let rightMetaKey	= 0xffe8;;
+    let leftAltKey	= 0xffe9;;
+    let rightAltKey	= 0xffea;;
+    let deleteKey	= 0xffff;;
+
+    external get_special: unit -> int*int*int*int = "get_special_keys";;
+
+    let leftAccKey, rightAccKey, leftCmdKey, rightCmdKey = get_special();; 
+
+end;;
+
+
 external event_dy: unit -> int = "event_dy";;
+external event_button: unit -> int = "event_button";;
+external set_event_clicks: int -> unit = "event_set_clicks";;
+external get_event_clicks: unit -> int = "event_get_clicks";;
+external event_is_click: unit -> bool = "event_is_click";;
+external event_set_is_click: bool -> unit = "event_set_is_click";;
+external event_clicks: unit -> int = "event_clicks";;
+external event_key: unit -> int = "event_key";;
+
+
+
+
