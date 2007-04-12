84a92190b824e45a534a411d4376a68dec7d5e1a
Micha <micha-1@fantasymail.de>
2007-04-13 00:28:35 +0200

    added resize demo

---
+++ resize.ml
@@ -0,0 +1,21 @@
+open Widgets;;
+
+let _ =
+    let window = new fWindow 320 65 "" in
+    let b1 = new fButton 0 0 320 65 "Resize Me" in
+    let cb =
+        let f = ref true in
+        fun () -> if !f then
+                      ignore (b1#resize ~xy:(100,10) 190 35)
+                  else
+                      ignore (b1#resize ~xy:(0,0) 320 65);
+                  f := not !f
+    in
+    let rec cb1 () = ignore (b1#resize ~xy:(100,10) 190 35); b1#callback cb2
+    and cb2 () = ignore (b1#resize ~xy:(0,0) 320 65); b1#callback cb1 in
+    b1#callback cb1;
+    window#wend;
+    window#show;
+    Run.run ()
+;;
+
